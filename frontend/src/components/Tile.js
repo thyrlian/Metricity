2a9c9e56f89a173799759ac423e60a3029f54acb
Jing Li <thyrlian@gmail.com>
2024-11-09 14:43:21 +0100

    Add type support (embed & image) to Tile component

--- frontend/src/components/Tile.js
+++ frontend/src/components/Tile.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 const Tile = ({ data }) => {
+    const renderContent = () => {
+        switch (data.type) {
+            case 'embed':
+                return <iframe src={data.url} title={data.name} width="100%" height="300px"></iframe>;
+            case 'image':
+                return <img src={data.url} alt={data.name} width="100%" />;
+            default:
+                return <div>Unsupported type: {data.type}</div>;
+        }
+    };
+
     return (
         <div className="tile">
             <h3>{data.name}</h3>
-            <iframe src={data.url} title={data.name} width="100%" height="300px"></iframe>
+            {renderContent()}
         </div>
     );
 };
